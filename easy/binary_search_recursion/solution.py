9877c72240b59809e72cb77e09f88fb474b40d7d
Yuvraj Joshi <yuvrajjoshi2003@gmail.com>
2022-10-23 01:27:50 +0530

    Final solution

--- easy/binary_search_recursion/solution.py
+++ easy/binary_search_recursion/solution.py
@@ -2,12 +2,12 @@ def solution(arr='',target=float('-inf')):
     if target==float('-inf'):return -1
     mid = len(arr) // 2
     if len(arr) == 1:
-        return mid if arr[mid] == target else None
+        return mid if arr[mid] == target else -1
     elif arr[mid] == target:
         return mid
     else:
         if arr[mid] < target:
             callback_response = solution((arr[mid:]), target)
-            return mid + callback_response if callback_response is not None else None
+            return mid + callback_response if callback_response is not -1 else -1
         else:
             return solution((arr[:mid]), target)
