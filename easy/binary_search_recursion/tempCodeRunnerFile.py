f506a24df4bea3777002ded40ee982426ec7c669
Yuvraj Joshi <yuvrajjoshi2003@gmail.com>
2022-10-22 20:44:52 +0530

    Done

---
+++ easy/binary_search_recursion/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+
+        "Normal Test 4"
+        self.assertEqual(solution([1, 2, 3, 4, 5], 0), -1, "Test 4 Failed")
+
+    def test_binary_search_test_e(self):
+        "Normal Test 5"
+        self.assertEqual(solution([1, 2, 3, 4, 5], 6), -1, "Test 5 Failed")
