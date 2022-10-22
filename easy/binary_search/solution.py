1437994e4ef7ddb07029289f6165c1a4e9c59816
Yuvraj Joshi <yuvrajjoshi2003@gmail.com>
2022-10-22 20:44:46 +0530

    Done.

--- easy/binary_search/solution.py
+++ easy/binary_search/solution.py
@@ -1,16 +1,2 @@
-def solution(nums='',target=float('-inf')):
-    if target==float('-inf'):return -1
-    if nums=='':return -1
-    left = 0
-    right = len(nums)-1
-
-    while left<=right:
-        mid = (left+right)//2
-        if nums[mid]==target:
-            return mid
-        elif nums[mid]>target:
-            right = mid-1
-        else:
-            left = mid+1
-
-    return -1
+def solution():
+    pass
