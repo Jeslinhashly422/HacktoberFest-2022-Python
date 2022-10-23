bea0a64f8aa91503d331b4757bd5fa19f2724498
Pranav Gupta <88652820+pranavgupta2603@users.noreply.github.com>
2022-10-23 22:41:43 +0530

    Changed directory of tower of hanoi solution

--- easy/tower_of_hanoi/solution.py
+++ easy/tower_of_hanoi/solution.py
@@ -1,2 +1,16 @@
-def solution():
-    pass
+def solution(n=None):
+    if n == None:
+        return 0
+    if isinstance(n, str):
+        try:
+            n = int(n)
+        except:
+            return 0
+    if n > 0 and isinstance(n, int):
+        try:
+            n = int(n)
+            return (2**int(n)) - 1
+        except:
+            return 0 
+    else:
+        return 0
--- easy/tower_of_hanoi_recursion/solution.py
+++ easy/tower_of_hanoi_recursion/solution.py
@@ -1,16 +1,2 @@
-def solution(n=None):
-    if n == None:
-        return 0
-    if isinstance(n, str):
-        try:
-            n = int(n)
-        except:
-            return 0
-    if n > 0 and isinstance(n, int):
-        try:
-            n = int(n)
-            return (2**int(n)) - 1
-        except:
-            return 0 
-    else:
-        return 0
+def solution():
+    pass
